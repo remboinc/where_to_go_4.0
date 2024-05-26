f207f2fa58ed82e064499cc8c2470f930631e9dd
Ekaterina Ryabova <58534082+remboinc@users.noreply.github.com>
2024-05-26 15:39:54 +0300

    Rename func()

--- places/views.py
+++ places/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def view(request):
+def index(request):
     return render(request, 'index.html')
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from where_to_go.views import view
+from places.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', view),
+    path('', index),
 ]
