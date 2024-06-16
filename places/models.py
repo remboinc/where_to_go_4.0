a97829a8c77951ac262676c41407d5d82c1c5144
Ekaterina Ryabova <58534082+remboinc@users.noreply.github.com>
2024-06-16 15:02:40 +0300

    Null False

--- places/models.py
+++ places/models.py
@@ -6,8 +6,8 @@ class Place(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название')
     short_description = models.TextField(verbose_name='Краткое описание', null=True, blank=True)
     long_description = HTMLField(verbose_name='Полное описание', null=True, blank=True)
-    lng = models.FloatField(default=0, verbose_name='Долгота')
-    lat = models.FloatField(default=0, verbose_name='Широта')
+    lng = models.FloatField(verbose_name='Долгота')
+    lat = models.FloatField(verbose_name='Широта')
 
     def __str__(self):
         return self.title
