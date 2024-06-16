2dbaf2e2716d42d1dc3938bb5a386a756c95bb02
Ekaterina Ryabova <58534082+remboinc@users.noreply.github.com>
2024-06-16 15:18:01 +0300

    Bug fix

--- places/admin.py
+++ places/admin.py
@@ -12,7 +12,7 @@ class ImageInline(SortableInlineAdminMixin, admin.StackedInline):
     fields = ('title', 'image', 'get_preview', 'order')
 
     def get_preview(self, obj):
-        return format_html('<img src="{}" height="200" />', mark_safe)
+        return format_html('<img src="{}" style="max-height: 200px; width: auto;" />', obj.image.url)
 
 
 
