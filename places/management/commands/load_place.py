2505f56a071ee11cf0909d7e49c6da9ae7d8d3a5
Ekaterina Ryabova <58534082+remboinc@users.noreply.github.com>
2024-06-19 22:18:22 +0300

    Update load_place.py

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,4 +1,3 @@
-import os
 import json
 import requests
 from django.core.files.base import ContentFile
@@ -6,8 +5,7 @@ from django.core.management.base import BaseCommand
 from places.models import Place, PlaceImage
 
 
-def get_image(templates):
-    images_urls = templates.get('imgs', [])
+def get_image(images_urls):
     images = []
     for image_url in images_urls:
         try:
@@ -20,32 +18,26 @@ def get_image(templates):
     return images
 
 
-def load_place_from_json(json_file):
+def load_place_from_url(url):
     try:
-        with open(json_file, 'r', encoding='utf-8') as file:
-            return json.load(file)
-    except Exception as e:
-        print(f"Ошибка при загрузке данных из файла {json_file}: {e}")
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при загрузке данных из URL {url}: {e}")
         return None
 
 
-def load_places_from_directory(directory):
-    for filename in os.listdir(directory):
-        if filename.endswith('.json'):
-            json_file = os.path.join(directory, filename)
-            template = load_place_from_json(json_file)
-            if template:
-                load_place_to_admin_panel(template)
-
-
 def load_place_to_admin_panel(template):
-    images = get_image(template)
+    images = get_image(template.get('imgs', []))
     place, created = Place.objects.get_or_create(
         title=template.get('title'),
-        description_short=template.get('description_short'),
-        description_long=template.get('description_long'),
-        lng=template.get('coordinates', {}).get('lng'),
-        lat=template.get('coordinates', {}).get('lat'),
+        defaults={
+            'description_short': template.get('description_short'),
+            'description_long': template.get('description_long'),
+            'lng': template.get('coordinates', {}).get('lng'),
+            'lat': template.get('coordinates', {}).get('lat'),
+        }
     )
 
     if created:
@@ -67,13 +59,13 @@ def load_place_to_admin_panel(template):
 
 
 class Command(BaseCommand):
+    help = 'Load place data from a JSON file URL'
+
     def add_arguments(self, parser):
-        parser.add_argument('directory', type=str, help='Путь к папке с JSON файлами')
+        parser.add_argument('url', type=str, help='The URL of the JSON file containing place data')
 
     def handle(self, *args, **kwargs):
-        directory = kwargs['directory']
-        if not os.path.exists(directory):
-            self.stdout.write(self.style.ERROR(f'Папка {directory} не существует.'))
-            return
-
-        load_places_from_directory(directory)
+        url = kwargs['url']
+        template = load_place_from_url(url)
+        if template:
+            load_place_to_admin_panel(template)
