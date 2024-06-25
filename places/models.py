92fbc95dcf49bcff20ac798eea2799994af072af
Ekaterina Ryabova <58534082+remboinc@users.noreply.github.com>
2024-06-25 23:50:31 +0300

    Added new migration

--- places/models.py
+++ places/models.py
@@ -4,8 +4,8 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название')
-    short_description = models.TextField(verbose_name='Краткое описание', null=True, blank=True)
-    long_description = HTMLField(verbose_name='Полное описание', null=True, blank=True)
+    short_description = models.TextField(verbose_name='Краткое описание', default='Без описания... пока', blank=True)
+    long_description = HTMLField(verbose_name='Полное описание', default='Без описания... пока', blank=True)
     lng = models.FloatField(verbose_name='Долгота')
     lat = models.FloatField(verbose_name='Широта')
 
