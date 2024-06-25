f1de6501acb11ef9fcd742f02eab54216e37561a
Ekaterina Ryabova <58534082+remboinc@users.noreply.github.com>
2024-06-25 23:35:23 +0300

    Update settings.py

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 SESSION_COOKIE_SECURE = True
