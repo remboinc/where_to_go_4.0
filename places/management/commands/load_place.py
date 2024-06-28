3a749f790e490c66dcdf463be355b6590aae43f0
Ekaterina Ryabova <58534082+remboinc@users.noreply.github.com>
2024-06-28 21:44:00 +0300

    Update load_place.py

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -6,34 +6,7 @@ from places.models import Place, PlaceImage
 from django.core.exceptions import MultipleObjectsReturned
 
 
-def get_images(images_urls):
-    images = []
-    for image_url in images_urls:
-        try:
-            response = requests.get(image_url)
-            response.raise_for_status()
-            b_image = response.content
-            images.append(b_image)
-            print(f'Изображение успешно загружено: {image_url}')
-        except requests.exceptions.RequestException as e:
-            print(f'Ошибка при загрузке изображения {image_url}: {e}')
-    return images
-
-
-def load_place_from_url(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        print(f'Данные успешно загружены из URL: {url}')
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        print(f'Ошибка при загрузке данных из URL {url}: {e}')
-    except json.JSONDecodeError as e:
-        print(f'Ошибка при парсинге JSON из URL {url}: {e}')
-
-
 def load_place_to_admin_panel(template):
-    images = get_images(template.get('imgs', []))
     try:
         place, created = Place.objects.get_or_create(
             title=template['title'],
@@ -48,16 +21,21 @@ def load_place_to_admin_panel(template):
         places = Place.objects.filter(title=template['title'])
         place = places.first()
         created = False
-        print(f'Найдено несколько мест с заголовком "{template.get("title")}". Используется первый найденный.')
+        print(f'Найдено несколько мест с заголовком "{template["title"]}". Используется первый найденный.')
 
     if created:
         print('Новый объект был создан.')
-        for index, b_image in enumerate(images):
-            image_name = f'{place.id}_{index}.jpg'
-            image_of_place = PlaceImage.objects.create(place=place)
+        for index, image_url in enumerate(template['imgs']):
             try:
+                response = requests.get(image_url)
+                response.raise_for_status()
+                b_image = response.content
+                image_name = f'{place.id}_{index}.jpg'
+                image_of_place = PlaceImage.objects.create(place=place)
                 image_of_place.image.save(image_name, ContentFile(b_image), save=True)
                 print(f'Изображение {image_name} успешно сохранено.')
+            except requests.exceptions.RequestException as e:
+                print(f'Ошибка при загрузке изображения {image_url}: {e}')
             except Exception as e:
                 print(f'Ошибка при сохранении изображения {image_name}: {e.__class__.__name__} - {e}')
     else:
@@ -74,6 +52,18 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         url = kwargs['url']
-        template = load_place_from_url(url)
+
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            print(f'Данные успешно загружены из URL: {url}')
+            template = response.json()
+        except requests.exceptions.RequestException as e:
+            print(f'Ошибка при загрузке данных из URL {url}: {e}')
+            return
+        except json.JSONDecodeError as e:
+            print(f'Ошибка при парсинге JSON из URL {url}: {e}')
+            return
+
         if template:
             load_place_to_admin_panel(template)
