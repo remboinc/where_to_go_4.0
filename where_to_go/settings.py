8d467f58e0a8208fce1f1624bcbd555a3452acf2
Ekaterina Ryabova <58534082+remboinc@users.noreply.github.com>
2024-07-01 13:54:01 +0300

    Added static

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -9,7 +9,7 @@ env.read_env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY', default='default-secret-key')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
